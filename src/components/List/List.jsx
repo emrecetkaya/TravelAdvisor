768ea145aba452ed6d537b9e531b7aa600486d3d
Emre Cetinkaya <emrecetinkaya@Emres-MBP.attlocal.net>
2022-08-24 22:29:06 -0700

    React project created / Header.jsx completed / Map.jsx worked / starting to working on List.jsx

---
+++ src/components/List/List.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from "@material-ui/core"
+
+import useStyles from './styles';
+
+const List = () => {
+    const classes = useStyles();
+
+    return (
+        <div>List</div>
+    )
+}
+
+export default List
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,33 @@
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import React from 'react'
+import GoogleMapReact from 'google-map-react';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import { Rating } from "@material-ui/lab";
+
+import useStyles from './styles';
+
+const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+
+    const coordinates = { lat: 0, lng: 0 };
+
+    return (
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyDHF_XPGnw0f0oocl9AAbgq1YKIQOiSQxU' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
+    )
+}
+
+export default Map
